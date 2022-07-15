de436a2a8cf32e2fd320c467fd2741776dc90b1b
Noah <23338687+enjay27@users.noreply.github.com>
2022-07-15 23:31:14 +0900

    feat: create comment

--- src/index.js
+++ src/index.js
@@ -87,6 +87,7 @@ const typeDefs = `
     type Mutation {
       createUser(name: String!, email: String!, age: Int): User!
       createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+      createComment(text: String!, author: ID!, post: ID!): Comment!
     }
 
     type User {
@@ -182,6 +183,30 @@ const resolvers = {
       posts.push(post);
 
       return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const userExists = users.some((user) => user.id === args.author);
+      if (!userExists) {
+        throw new Error("User not found");
+      }
+
+      const postExists = posts.some(
+        (post) => post.published && post.id === args.post
+      );
+      if (!postExists) {
+        throw new Error("No post");
+      }
+
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post,
+      };
+
+      comments.push(comment);
+
+      return comment;
     },
   },
   Post: {
